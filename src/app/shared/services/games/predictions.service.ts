0af472385907acf1e7fe6f207c05ded143b253e8
didz <didier.verloppe@gmail.com>
2026-06-11 00:31:46 +0400

    feat: implement prediction service and match interaction components for pronostics page

--- src/app/shared/services/games/predictions.service.ts
+++ src/app/shared/services/games/predictions.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, map, switchMap, tap, throwError } from 'rxjs';
+import { Observable, BehaviorSubject, map, switchMap, tap, throwError } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { environment } from '../../../../environments/environment';
 import { Pronostiques } from '../../contracts/pronostiques.contract';
@@ -14,6 +14,28 @@ export class PredictionsService {
   private cookieService = inject(CookieService);
   private prodUrl: string = environment.apiBaseUrl;
 
+  private draftsSubject = new BehaviorSubject<any[]>([]);
+  drafts$ = this.draftsSubject.asObservable();
+
+  addDraft(prediction: any): void {
+    const current = this.draftsSubject.getValue();
+    const index = current.findIndex(p => p.game_id === prediction.game_id);
+    if (index > -1) {
+      current[index] = prediction;
+    } else {
+      current.push(prediction);
+    }
+    this.draftsSubject.next([...current]);
+  }
+
+  getDrafts(): any[] {
+    return this.draftsSubject.getValue();
+  }
+
+  clearDrafts(): void {
+    this.draftsSubject.next([]);
+  }
+
   sendPrediction(predictions: Pronostiques): Observable<any> {
     let token = this.cookieService.get('currentToken');
   
@@ -24,7 +46,11 @@ export class PredictionsService {
           'Authorization': `Bearer ${token}`
         })
       };
-      return this.httpClient.post(`${this.prodUrl}/items/pronostiques`, predictions, httpOptions);
+      if (predictions.id) {
+        return this.httpClient.put(`${this.prodUrl}/items/pronostiques/${predictions.id}`, predictions, httpOptions);
+      } else {
+        return this.httpClient.post(`${this.prodUrl}/items/pronostiques`, predictions, httpOptions);
+      }
     } else {
       return throwError('No token found');
     }
